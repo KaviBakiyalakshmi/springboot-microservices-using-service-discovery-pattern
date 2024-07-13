090adb147edd5c90a38de5c5f7a2909f9a44f02e
KaviBakiyalakshmi <ivak.srmist@gmail.com>
2024-07-13 10:02:27 +0530

    first commit

---
+++ springboot-home-microservies/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,25 @@
+
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HomeController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		String products =restTemplate.getForObject("http://SPRINGBOOT-PRODUCTS-MICROSERVIES/products", String.class);
+	
+		return products;
+		
+	}
+
+}
+
---
+++ springboot-home-microservies/src/main/java/com/example/demo/SpringbootHomeMicroserviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHomeMicroserviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHomeMicroserviesApplication.class, args);
+	}
+
+}
